2ada4205804ba55ba28f83983d4110290bc447f1
Bharath Muvva <bharathmuvva2003@gmail.com>
2026-03-09 20:16:08 +0530

    Updated About Page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,7 +2,7 @@ function About() {
   return (
     <div className="section">
       <div className="card">
-        <h2>About ObesityCare AI</h2>
+        <h1>About ObesityCare AI</h1>
 
         <div style={{ marginTop: "25px", lineHeight: "1.9" }}>
           <p>
@@ -25,6 +25,11 @@ function About() {
           <p style={{ marginTop: "15px" }}>
             This application demonstrates integration of React frontend and
             Node.js backend to simulate a scalable health monitoring system.
+          </p>
+
+          <h2>About Author</h2>
+          <p style={{ marginTop: "15px" }}>
+            Hello, I’m Muvva Sai Bharath, a final-year B.Tech engineering student with a strong interest in artificial intelligence, machine learning, and full-stack web development. I enjoy building practical technology solutions that combine data science with modern web applications. My work focuses on developing intelligent systems that can analyze real-world data and provide meaningful insights. One of my projects, ObesityCare AI, is designed to help analyze lifestyle factors and predict obesity risk using health-related inputs such as BMI, activity level, diet, sleep, and hydration. Through projects like this, I aim to apply machine learning concepts to solve real-world health and technology challenges while continuously improving my skills in software development, data analysis, and AI-driven solutions.
           </p>
         </div>
       </div>
